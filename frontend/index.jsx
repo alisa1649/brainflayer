0e22e1b446bf223daad8b3d5fbff2702e2bad61a
alisa1649 <alisa1649@gmail.com>
2021-03-15 18:25:41 -0400

    Added static pages controller

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 
-/*
-Write an entry point file that renders the `Root` component, with a `store`
-prop passed in, inside the div with id 'root'.
-*/
 document.addEventListener("DOMContentLoaded", () => {
     ReactDOM.render(<Root store={configureStore()} />, document.getElementById("root"))
 })
