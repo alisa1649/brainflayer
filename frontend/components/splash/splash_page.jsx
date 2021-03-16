74917fcee8940dcf6e0d139367ccee4e1554c84b
alisa1649 <alisa1649@gmail.com>
2021-03-16 17:04:59 -0400

    Added signup functionality

--- frontend/components/splash/splash_page.jsx
+++ frontend/components/splash/splash_page.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import SignupForm from '../session_form/signup_form.jsx';
 
 class SplashPage extends React.Component {
     render() {
         return (
             <div>
                 Hi I'm splashpage
+                <SignupForm />
             </div>
         )
     }
--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,5 +1,12 @@
 import { combineReducers } from 'redux';
+import errors from './errors_reducer';
+import session from './session_reducer';
+import users from "./users_reducer";
 
-const RootReducer = combineReducers({});
+const rootReducer = combineReducers({
+    users,
+    session,
+    errors,
+});
 
-export default RootReducer;
+export default rootReducer;
