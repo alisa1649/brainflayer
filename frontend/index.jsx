8e7e921f1e46d864e9f55909ec7c58ed6f1f3cbc
alisa1649 <alisa1649@gmail.com>
2021-03-16 17:04:59 -0400

    Added signup functionality

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -4,6 +4,22 @@ import Root from './components/root';
 import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
-    const store = configureStore()
+    let store;
+    if (window.currentUser) {
+        const preloadedState = {
+            session: { id: window.currentUser.id },
+            users: { [window.currentUser.id]: window.currentUser }
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
     ReactDOM.render(<Root store={store} />, document.getElementById("root"))
-})
+
+
+    // make state available for debugging
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+    window.ajax = $.ajax;
+});
