ba6ee6acc2fef8a99b67fd7fd4397dbf79cfccaa
alisa1649 <alisa1649@gmail.com>
2021-03-16 21:45:55 -0400

    Added login functionality

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import SplashPage from "./splash/splash_page"
+import SignupForm from './session_form/signup_form_container';
+import LoginForm from './session_form/login_form_container';
 
 // import containers here
 
@@ -11,7 +13,16 @@ const App = ({ children }) => (
         <Switch>
             <Route exact path="/" component={SplashPage} />
         </Switch>
-    </div>
+
+        <hr />
+        <h1>Testing Area</h1>
+        <div style={{ border: "solid black 2px" }}>
+            <SignupForm />
+        </div>
+        <div style={{ border: "solid black 2px" }}>
+            <LoginForm />
+        </div>
+    </div >
 );
 
 export default App;
--- frontend/components/splash/splash_page.jsx
+++ frontend/components/splash/splash_page.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import SignupForm from '../session_form/signup_form.jsx';
 
 class SplashPage extends React.Component {
     render() {
         return (
             <div>
                 Hi I'm splashpage
-                <SignupForm />
             </div>
         )
     }
