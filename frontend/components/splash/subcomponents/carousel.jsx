d72d8c97d4e5d529b4ac9911c4f8586547c2720b
alisa1649 <alisa1649@gmail.com>
2021-03-18 12:42:48 -0400

    Added hover to buttons

--- frontend/components/splash/subcomponents/carousel.jsx
+++ frontend/components/splash/subcomponents/carousel.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 export default () => {
     return (
-        <div class="carousel">
-            <div class="carousel-slide background-1">
-                <div class="overlay"></div>
+        <div className="carousel">
+            <div className="carousel-slide background-1">
+                <div className="overlay"></div>
             </div>
         </div>
     )
