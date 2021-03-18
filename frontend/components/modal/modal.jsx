c017c125ac30c507ce53cffa4f79b1cbddab84b6
alisa1649 <alisa1649@gmail.com>
2021-03-18 16:25:07 -0400

    Added close buttons to modals

--- frontend/components/modal/modal.jsx
+++ frontend/components/modal/modal.jsx
@@ -5,6 +5,8 @@ class Modal extends React.Component {
         return (
             <div className="modal-overlay" onClick={this.props.closeModal}>
                 <div className="modal-form-container" onClick={e => e.stopPropagation()}>
+                    <span className="modal-x" onClick={this.props.closeModal}>
+                    </span>
                     {this.props.children}
                 </div>
             </div>
