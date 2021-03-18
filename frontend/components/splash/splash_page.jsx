ec6d22e07d101985b9a7cfe59ace780f8d1e91ba
alisa1649 <alisa1649@gmail.com>
2021-03-18 11:22:43 -0400

    Added placeholder for splash carousel

--- frontend/components/splash/splash_page.jsx
+++ frontend/components/splash/splash_page.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from '../navbar/navbar';
 import LoginForm from '../login_form/login_form';
 import SignupForm from '../signup_form/signup_form';
+import Carousel from './subcomponents/carousel';
 
 class SplashPage extends React.Component {
     render() {
         return (
             <div>
                 <Navbar />
+                <Carousel />
                 <LoginForm />
                 <SignupForm />
             </div>
---
+++ frontend/components/splash/subcomponents/carousel.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+export default () => {
+    return (
+        <div class="carousel">
+            <div class="carousel-slide background-1">
+                <div class="overlay"></div>
+            </div>
+        </div>
+    )
+
+}
