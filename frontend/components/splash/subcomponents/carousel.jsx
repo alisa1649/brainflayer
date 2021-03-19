7e31c2027f9188b8d850ae824f38723412a13889
alisa1649 <alisa1649@gmail.com>
2021-03-19 17:20:19 -0400

    Added background text to carousel

--- frontend/components/splash/subcomponents/carousel.jsx
+++ frontend/components/splash/subcomponents/carousel.jsx
@@ -2,6 +2,15 @@ import React from 'react';
 export default () => {
     return (
         <div className="carousel">
+            <div className="marketing">
+                <div className="hero">Rise to<br />your challenge.</div>
+                <div className="hero-sub">Flashcards for <span className="bold">serious learners.</span></div>
+                <div className="button-row">
+                    <div className="square-button blue">Find Flashcards</div>
+                    <div className="square-button">Make Flashcards</div>
+                    <div className="watch-button">Watch Video</div>
+                </div>
+            </div>
             <div className="carousel-slide background-1">
                 <div className="overlay"></div>
             </div>
