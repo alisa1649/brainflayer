184cb482b47b4bde47624c2e2edcca8426f101f2
alisa1649 <alisa1649@gmail.com>
2021-03-18 21:16:55 -0400

    Added navigation items styling

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -9,22 +9,52 @@ class NavBar extends React.Component {
                 <div className="logo-container">
                     <div className="logo"></div>
                 </div>
-                <div className="actions">
-                    <div className="action login" onClick={this.props.openLoginModal}>
-                        Log In
+
+                <span className="items">
+                    <div className="options">
+                        <div className="option find-flashcards">
+                            Find Flashcards
                     </div>
-                    <div className="action upgrade" onClick={this.props.openSignupModal}>
-                        Get Started
+                        <div className="option make-flashcards">
+                            Make Flashcards
                     </div>
-                </div>
+                        <div className="option why-works">
+                            Why It Works
+                    </div>
+                        <div className="option learn-more">
+                            More...
+                    </div>
+                    </div>
+
+                    {
+                        this.props.loggedIn
+                            ? <div className="actions">
+                                <div className="action login" onClick={this.props.openLoginModal}>
+                                    {this.props.email}
+                                </div>
+                                <div className="action upgrade" onClick={this.props.openSignupModal}>
+                                    Upgrade To Pro
+                            </div>
+                            </div>
+                            : <div className="actions">
+                                <div className="action login" onClick={this.props.openLoginModal}>
+                                    Log In
+                            </div>
+                                <div className="action upgrade" onClick={this.props.openSignupModal}>
+                                    Get Started
+                            </div>
+                            </div>
+                    }
+                </span>
             </div>
         )
     }
 }
 const mapStateToProps = (state) => {
     return {
-        // TODO: add current user login status here 
-    };
+        loggedIn: Boolean(state.session.id),
+        email: state.users[state.session.id].email
+    }
 };
 
 const mapDispatchToProps = dispatch => {
