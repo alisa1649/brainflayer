17c348b9cccac404ee04183636c9e7d0b8ada0e5
alisa1649 <alisa1649@gmail.com>
2021-03-22 09:40:05 -0400

    Added back end functionality for cards, tags, decks

---
+++ app/controllers/api/decks_controller.rb
@@ -0,0 +1,35 @@
+class Api::DecksController < ApplicationController
+  before_action :require_logged_in, only: [:create, :study, :delete, :index] # TODO: add constraints here
+
+  def create
+    @deck = Deck.new(deck_params)
+    @deck.author_id = current_user.id
+    
+    if @deck.save!
+      render :show, status: :created
+    else
+      render json: @deck.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @decks = current_user.decks
+    render json: @decks
+  end
+
+  def show
+    @deck = Deck.find(params[:id])
+    #TODO: add associated cards here
+  end
+
+  def destroy
+    @deck = Deck.find(params[:id])
+    @deck.destroy!
+    render json: @deck
+  end
+
+  def deck_params
+    params.require(:deck).permit(:title, :objective, :tag_id)
+  end
+
+end
