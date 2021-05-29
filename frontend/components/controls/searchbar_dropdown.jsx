0848bbb230adcfce76c2c4398a33a6b44c7f751e
alisa1649 <alisa1649@gmail.com>
2021-05-29 19:31:52 -0400

    Added styling to searchbar dropdown component

--- frontend/components/controls/searchbar_dropdown.jsx
+++ frontend/components/controls/searchbar_dropdown.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import SearchBar from "../search/SearchBar";
+import {Link} from "react-router-dom";
+
 class SearchbarDropdown extends React.Component {
     constructor(props) {
         super(props)
@@ -9,6 +11,24 @@ class SearchbarDropdown extends React.Component {
         return (
             <div className="dropdown-container">
                 <SearchBar />
+                <div className="dropdown-content">
+                    <div className="dropdown-close">
+                    </div>
+                    <div className="dropdown-title">
+                        <span className="dropdown-heading">
+                            BrainFlayer's Knowledge Genome
+                        </span>
+                    </div>
+                    <div className="dropdown-subtitle">
+                        Browse over 1 million classes created by Alisa!
+                    </div>
+                    <ul className="dropdown-subjects">
+                        <li className="subject-item">
+                            <Link to="/landing" className="dropdown-item-link"></Link>
+                        </li>
+
+                    </ul>
+                </div>
             </div>
         )
     }
